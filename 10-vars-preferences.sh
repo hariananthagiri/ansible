d949dc791fbab7493cab944a6a23b2f25b3a3f53
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-24 13:53:11 +0530

    ansible example

--- 10-vars-preferences.sh
+++ 10-vars-preferences.sh
@@ -3,13 +3,14 @@
   tasks:
   - name: printing variable preferences
     ansible.builtin.debug:
-      msg: "1.variable preference is COMMAND LINE"
-      msg: "2.variable preference is TASK"
-      msg: "3.variable preference is FILE"
-      msg: "4.variable preference is PROMPT"
-      msg: "5.variable preference is PLAY"
-      msg: "6.variable preference is INVENTORY"
-      msg: "7.variable preference is ROLES"
+      msg: 
+      - "1.variable preference is COMMAND LINE"
+      - "2.variable preference is TASK"
+      - "3.variable preference is FILE"
+      - "4.variable preference is PROMPT"
+      - "5.variable preference is PLAY"
+      - "6.variable preference is INVENTORY"
+      - "7.variable preference is ROLES"
 
 # GIVEING VARIABLE PREFERENCES AS FOR IT HIGHERARKEY...
 
